98e2136c486741adb5819a1a4b081d0ad530f3a3
JafetSam <JafetSam7@gmail.com>
2017-11-07 12:22:11 -0600

    Otro commit

--- organizador/urls.py
+++ organizador/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     url(r'^lista_persona/$', views.lista_persona, name='lista_persona'),
     url(r'^base_actividad/$', views.base_actividad, name='base_actividad'),
     url(r'^lista_actividad/$', views.lista_actividad, name='lista_actividad'),
+    url(r'^nueva_persona/$', views.nueva_persona, name='nueva_persona'),
+    url(r'^nueva_actividad/$', views.nueva_actividad, name='nueva_actividad'),
+    url(r'^editar_persona/(?P<pk>[0-9]+)/$', views.editar_persona, name='editar_persona'),
+    url(r'^editar_actividad/(?P<pk>[0-9]+)/$', views.editar_actividad, name='editar_actividad'),
+    url(r'^eliminar_persona/(?P<pk>[0-9]+)/$', views.eliminar_persona, name='eliminar_persona'),
+    url(r'^eliminar_actividad/(?P<pk>[0-9]+)/$', views.eliminar_actividad, name='eliminar_actividad'),
     ]
--- organizador/views.py
+++ organizador/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib import messages
-from .forms import PersonaForm
-from organizador.models import Persona, Actividad
-
+from .forms import PersonaForm,ActividadForm
+from organizador.models import Persona, Actividad,TareasPersona
 
 
 def base(request):
@@ -21,3 +20,90 @@ def base_actividad(request):
 def lista_actividad(request):
     actividades=Actividad.objects.all()
     return render(request,'organizador/lista_actividad.html',{'actividades':actividades})
+
+
+
+def nueva_persona(request):
+    if request.method == "POST":
+        formulario = PersonaForm(request.POST)
+        if formulario.is_valid():
+
+            persona = Persona.objects.create(nombre=formulario.cleaned_data['nombre'], apellido= formulario.cleaned_data['apellido'])
+
+            for actividad_id in request.POST.getlist('actividades'):
+
+                t =TareasPersona(actividad_id=actividad_id, persona_id = persona.id)
+                t.save()
+
+            return redirect('lista_persona')
+    else:
+        formulario = PersonaForm()
+    return render(request, 'organizador/nueva_persona.html', {'formulario': formulario})
+
+
+def nueva_actividad(request):
+    if request.method == "POST":
+        formulario = ActividadForm(request.POST)
+        if formulario.is_valid():
+
+            acitividad = Actividad.objects.create(
+            nombre=formulario.cleaned_data['nombre'],
+            descripcion= formulario.cleaned_data['descripcion'],
+            fecha=formulario.cleaned_data['fecha'],
+            hora=formulario.cleaned_data['hora'],
+            )
+
+
+            return redirect('lista_actividad')
+    else:
+        formulario = ActividadForm()
+    return render(request, 'organizador/nueva_actividad.html', {'formulario': formulario})
+
+
+
+
+
+
+def editar_persona(request,pk):
+    persona=get_object_or_404(Persona,pk=pk)
+    if request.method =="POST":
+        form=PersonaForm(request.POST,instance=persona)
+
+        if form.is_valid():
+            persona=form.save(commit=False)
+            persona.save()
+            return redirect('lista_persona')
+    else:
+        form=PersonaForm(instance=persona)
+    return render(request,'organizador/editar_persona.html',{'form':form})
+
+
+
+
+def editar_actividad(request,pk):
+    actividad=get_object_or_404(Actividad,pk=pk)
+    if request.method =="POST":
+        form=ActividadForm(request.POST,instance=actividad)
+
+        if form.is_valid():
+            actividad=form.save(commit=False)
+            actividad.save()
+            return redirect('lista_actividad')
+    else:
+        form=ActividadForm(instance=actividad)
+    return render(request,'organizador/editar_actividad.html',{'form':form})
+
+
+
+
+
+def eliminar_persona(request,pk):
+    persona=get_object_or_404(Persona,pk=pk)
+    persona.delete()
+    return redirect('lista_persona')
+
+
+def eliminar_actividad(request,pk):
+    actividad=get_object_or_404(Actividad,pk=pk)
+    actividad.delete()
+    return redirect('lista_actividad')
